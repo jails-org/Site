4762ed4c446b34fd184c6c9361783997148e5061
Javiani <edudu_tata@hotmail.com>
2023-11-11 22:40:53 -0300

    Several performance improvements

--- src/shared/_utils/index.ts
+++ src/shared/_utils/index.ts
@@ -13,3 +13,26 @@ export const isVisible = (elem, offsetTop = 0) => {
 	const viewport = (window.innerHeight || document.documentElement.clientHeight)
 	return bounding.top <= viewport - offsetTop
 }
+
+export const thirdParty = (name, timeout = 100) => {
+
+	return new Promise((resolve, reject) => {
+		const script = document.querySelector(`script[data-name=${name}]`)
+		if (!script) {
+			reject(`[ThirdParty] -> script for data-name="${name}" not found.`)
+		} else {
+			if( script.src ) {
+				const newscript = document.createElement('script')
+				newscript.addEventListener('load', () => {
+					eval( script.text )
+					resolve( newscript )
+				})
+				newscript.src = script.src
+				document.head.appendChild(newscript)
+			}else {
+				eval(script.text)
+				resolve(script)
+			}
+		}
+	})
+}
--- src/shared/main.ts
+++ src/shared/main.ts
@@ -1,10 +1,13 @@
 import jails from 'jails-js'
+import { thirdParty } from './_utils'
 
 export const register = jails.register
 export const dependencies = {}
 
 export default function main (){
+
     document.addEventListener('DOMContentLoaded', () => {
 		jails.start()
+		thirdParty('analytics')
     })
 }
