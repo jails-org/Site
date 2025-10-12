b788ea3757f0a0c3d2c5d9b834fe125574c95ba8
Javiani <eduardo.ottaviani@gmail.com>
2025-10-12 16:45:32 -0300

    Upgrading tailwind to version 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	devToolbar: {
-		enabled: false
+  devToolbar: {
+    enabled: false
 	},
-	integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
--- tailwind.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,ts}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
