58bcbcc7286e1264603813c93e33c6fe52a4593a
Javiani <eduardo.ottaviani@gmail.com>
2026-03-15 04:11:20 -0300

    Fixing primsjs dependencies

--- src/domain/home-v2/components/code/index.ts
+++ src/domain/home-v2/components/code/index.ts
@@ -1,13 +1,14 @@
 import { type Component } from 'jails-js'
-
-import Prism from 'prismjs'
+import { importJs } from 'jails.stdlib/import-js'
+import { importCss } from 'jails.stdlib/import-css'
 
 export default async function appCode ({ main }: Component ) {
 	
-	await import('prismjs/themes/prism-tomorrow.css')
-	await import('prismjs/components/prism-javascript')
+	await importCss('https://cdn.jsdelivr.net/npm/prismjs@1.30.0/themes/prism-tomorrow.min.css')
+	await importJs('https://cdn.jsdelivr.net/npm/prismjs/prism.min.js')
+	await importJs('https://cdn.jsdelivr.net/npm/prismjs/components/prism-javascript.min.js')
 	
 	main(() => {
-		Prism.highlightAll()
+		window.Prism.highlightAll()
 	})
 }
