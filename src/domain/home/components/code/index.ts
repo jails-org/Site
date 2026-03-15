49234d318592a6aa43202b56bb6341988148056d
Javiani <eduardo.ottaviani@gmail.com>
2026-03-15 05:08:14 -0300

    trying to fix cache issues

--- src/domain/home/components/code/index.ts
+++ src/domain/home/components/code/index.ts
@@ -5,8 +5,8 @@ import { importCss } from 'jails.stdlib/import-css'
 export default async function appCode ({ main }: Component ) {
 	
 	importCss('https://cdn.jsdelivr.net/npm/prismjs@1.30.0/themes/prism-tomorrow.min.css')
-	await importJs('https://cdn.jsdelivr.net/npm/prismjs/prism.min.js')
-	importJs('https://cdn.jsdelivr.net/npm/prismjs/components/prism-javascript.min.js')
+	await importJs('https://cdn.jsdelivr.net/npm/prismjs/prism.min.js?v=' + Math.random())
+	importJs('https://cdn.jsdelivr.net/npm/prismjs/components/prism-javascript.min.js?v=' + Math.random() )
 
 	main(() => {
 		window.Prism.highlightAll()
