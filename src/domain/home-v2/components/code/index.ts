98056970e979caa04c56a4ced0c772fc2a3813ae
Javiani <eduardo.ottaviani@gmail.com>
2026-03-15 04:28:15 -0300

    Fixing concurrency

--- src/domain/home-v2/components/code/index.ts
+++ src/domain/home-v2/components/code/index.ts
@@ -4,17 +4,11 @@ import { importCss } from 'jails.stdlib/import-css'
 
 export default async function appCode ({ main }: Component ) {
 	
-	await importHighlightLibrary()
-	
+	importCss('https://cdn.jsdelivr.net/npm/prismjs@1.30.0/themes/prism-tomorrow.min.css')
+	await importJs('https://cdn.jsdelivr.net/npm/prismjs/prism.min.js')
+	importJs('https://cdn.jsdelivr.net/npm/prismjs/components/prism-javascript.min.js')
+
 	main(() => {
 		window.Prism.highlightAll()
 	})
-}
-
-const importHighlightLibrary = () => {
-	return Promise.all([
-		importCss('https://cdn.jsdelivr.net/npm/prismjs@1.30.0/themes/prism-tomorrow.min.css'),
-		importJs('https://cdn.jsdelivr.net/npm/prismjs/prism.min.js'),
-		importJs('https://cdn.jsdelivr.net/npm/prismjs/components/prism-javascript.min.js')
-	])
 }
