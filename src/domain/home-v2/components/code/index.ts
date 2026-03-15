66dbd15979b4067db09ce3763f2881fb2d01e769
Javiani <eduardo.ottaviani@gmail.com>
2026-03-15 04:15:11 -0300

    Updating cdn library

--- src/domain/home-v2/components/code/index.ts
+++ src/domain/home-v2/components/code/index.ts
@@ -4,11 +4,17 @@ import { importCss } from 'jails.stdlib/import-css'
 
 export default async function appCode ({ main }: Component ) {
 	
-	await importCss('https://cdn.jsdelivr.net/npm/prismjs@1.30.0/themes/prism-tomorrow.min.css')
-	await importJs('https://cdn.jsdelivr.net/npm/prismjs/prism.min.js')
-	await importJs('https://cdn.jsdelivr.net/npm/prismjs/components/prism-javascript.min.js')
+	await importHighlightLibrary()
 	
 	main(() => {
 		window.Prism.highlightAll()
 	})
+}
+
+const importHighlightLibrary = () => {
+	return Promise.all([
+		importCss('https://cdn.jsdelivr.net/npm/prismjs@1.30.0/themes/prism-tomorrow.min.css'),
+		importJs('https://cdn.jsdelivr.net/npm/prismjs/prism.min.js'),
+		importJs('https://cdn.jsdelivr.net/npm/prismjs/components/prism-javascript.min.js')
+	])
 }
