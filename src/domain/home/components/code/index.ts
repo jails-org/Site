b6e2109382a44ffd6cf30ef806162d01271093cb
Javiani <eduardo.ottaviani@gmail.com>
2026-03-15 17:36:49 -0300

    Loading hightlightjs via cdn

--- src/domain/home/components/code/index.ts
+++ src/domain/home/components/code/index.ts
@@ -1,16 +1,22 @@
 import { type Component } from 'jails-js'
+import { importCss } from 'jails.stdlib/import-css'
+import { importJs }  from 'jails.stdlib/import-js'
+import { isVisible } from 'jails.stdlib/is-visible'
 
-import hljs  from 'highlight.js/lib/core'
-import ts 	 from 'highlight.js/lib/languages/typescript'
-import 'highlight.js/styles/atom-one-light.min.css'
-
-hljs.registerLanguage('typescript', ts)
+const CDN = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0'
 
 export default async function appCode ({ main, elm }: Component ) {
 	
-	const code = elm.querySelector('code')
+	await isVisible( elm )	
+
+	importCss(`${CDN}/styles/atom-one-light.min.css`)
+
+	await importJs(`${CDN}/highlight.min.js`)
+	await importJs(`${CDN}/languages/javascript.min.js`)
 
 	main(() => {
-		hljs.highlightElement(code)
+		const code = elm.querySelector('code')
+		//@ts-ignore
+		window.hljs.highlightElement( code )
 	})
 }
