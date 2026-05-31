68357c09e8d68c922d1728b40347883af668cc6c
Javiani <eduardo.ottaviani@gmail.com>
2026-05-31 00:48:30 -0300

    Removing lazyloading

--- src/shared/application.ts
+++ src/shared/application.ts
@@ -1,6 +1,5 @@
 import { type Component } from 'jails-js'
 import { thirdParty } from 'jails.stdlib/third-party'
-import { Lazyload } from 'jails.stdlib/lazyload-image' 
 
 export default function application ({ main } : Component) {
 
@@ -11,7 +10,6 @@ export default function application ({ main } : Component) {
 	})
 
 	const onload = () => {
-		new Lazyload({ elements_selector: 'img[data-src]' })
 		analytics.then( _ => console.info('application/onload - Executing after analytics loaded') )
 	}
 
